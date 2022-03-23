40da2bd8247dafebded2126a672488d9ca490085
aparna124 <41099165+aparna124@users.noreply.github.com>
2022-03-23 19:49:15 -0400

    Done with UI, state, and implemented list using structural directives

---
+++ team-generator/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  newMemberName = '';
+  members: string[] = [];
+
+  onInput(member: string)
+  {
+    this.newMemberName = member;
+    console.log(this.newMemberName);
+  }
+
+  addMember()
+  {
+    this.members.push(this.newMemberName);
+  }
+
+}
