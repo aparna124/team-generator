affde2abcededc4f2fd511a34bfdfe5631bcc117
aparna124 <41099165+aparna124@users.noreply.github.com>
2022-03-24 16:47:10 -0400

    Done with team generator functionality

--- team-generator/src/app/app.component.ts
+++ team-generator/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { IfStmt } from '@angular/compiler';
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,6 +9,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
   newMemberName = '';
   members: string[] = [];
+  errorMessage = '';
+  numberOfTeams: number | string = '';
+  teams: string[][] = [[]]
 
   onInput(member: string)
   {
@@ -15,9 +19,64 @@ export class AppComponent {
     console.log(this.newMemberName);
   }
 
+  onNumberOfTeamsInput(value: string)
+  {
+    this.numberOfTeams = Number(value);
+  }
+
   addMember()
   {
+    if(!this.newMemberName)
+    {
+      this.errorMessage = "Name can't be empty";
+      return 
+    }
+
+    this.errorMessage = '';
     this.members.push(this.newMemberName);
+    this.newMemberName = '';
+  }
+
+  generateTeams()
+  {
+    if(!this.numberOfTeams || this.numberOfTeams <=0)
+    {
+      this.errorMessage = "Invalid number of teams"
+      return
+    }
+
+    if(this.members.length < this.numberOfTeams)
+    {
+      this.errorMessage = "Not enough members"
+      return
+    }
+
+    this.errorMessage = ""
+    const allMembers = [...this.members]
+
+
+    while(allMembers.length)
+    {
+      for(let i=0; i<this.numberOfTeams; i++)
+      {
+        const randomIndex = Math.floor(Math.random() * allMembers.length)
+        const member = allMembers.splice(randomIndex, 1)[0];
+        if(!member)break;
+        if(this.teams[i]){
+          this.teams[i].push(member)
+        }
+        else
+        {
+          this.teams[i] = [member]
+        }
+      }
+    }
+
+    this.members = [];
+    this.numberOfTeams = '';
+
+   
+    console.log(this.teams);
   }
 
 }
